dcce918ef571a464c237c256e303676574da3d72
Eva813 <as45986@gmail.com>
2022-04-10 14:46:59 +0800

    navbar , and swiper

---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<style lang="scss" scoped>
+@import "~@/assets/all.scss";
+.nav-collapse {
+  flex-basis: 100%;
+  flex-grow: 0 !important;
+  align-items: center;
+}
+</style>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">台灣走走 Tai Walk</a>
+      <!-- <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button> -->
+      <div id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">探索景點</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">節慶活動</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">品嚐美食</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+</script>
---
+++ src/components/Swiper.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="container">
+    <swiper :navigation="true" :modules="modules" class="mySwiper">
+      <swiper-slide>
+        <img src="@/assets/image/mountain.jpg" alt="" srcset="" />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="@/assets/image/central.jpg" alt="" srcset="" />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="@/assets/image/taipei.jpg" alt="" srcset="" />
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+
+import "swiper/css/navigation";
+
+//import "./style.css";
+
+// import required modules
+import { Navigation } from "swiper";
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Navigation],
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.container {
+  height: 450px;
+}
+// #app {
+//   height: 100%;
+// }
+// html,
+// body {
+//   position: relative;
+//   height: 100%;
+// }
+
+// body {
+//   background: #eee;
+//   font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+//   font-size: 14px;
+//   color: #000;
+//   margin: 0;
+//   padding: 0;
+// }
+
+.swiper {
+  width: 100%;
+  height: 60%;
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 40px;
+  background: #fff;
+  width: 100%;
+  height: 100%;
+
+  /* Center slide text vertically */
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  -webkit-align-items: center;
+  align-items: center;
+}
+
+.swiper-slide img {
+  //display: block;
+  width: 100%;
+  height: 100%;
+  margin: auto;
+  vertical-align: middle;
+  object-fit: cover;
+  border-radius: 24px;
+  object-position: 65% 58%;
+}
+</style>
