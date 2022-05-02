236347fa110a2fe183f7a8ec75501acb58e8b38a
Eva813 <as45986@gmail.com>
2022-05-02 13:44:40 +0800

    add active slot

---
+++ src/components/ActiveSlot.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="active-slot info bg-light">
+    <slot>
+      <p>活動時間： <slot name="time"> </slot></p>
+      <p>服務電話：<slot name="phone"> </slot></p>
+      <p>主辦單位：<slot name="org"> </slot></p>
+      <p>活動地點：<slot name="location"> </slot></p>
+      <p>官方網站：<slot name="site"> </slot></p>
+    </slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "~@/assets/all.scss";
+.info {
+  padding: 10px;
+  text-align: left;
+}
+</style>
