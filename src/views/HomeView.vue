413b3ac23d9c5582188f9b2faa7a6be3adae6aaf
Eva813 <as45986@gmail.com>
2022-05-02 13:22:09 +0800

    get hotData

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,26 +13,22 @@
           </div>
         </div>
       </div>
-      <div class="col-5 selection-part flex-column pt-4">
-        <div class="dropdown">
-          <button
-            class="btn btn-secondary dropdown-toggle dropdown-btn rounded"
-            type="button"
-            id="dropdownMenuButton1"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
+      <div class="col-5 selection-part flex-column pt-4 justify-content-start">
+        <div class="dropdown-section">
+          <select
+            class="select-items rounded-3"
+            v-model="city"
+            @change="handleSelect(city)"
           >
-            Dropdown button
-          </button>
-          <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+            <option disabled value="">請選擇縣市</option>
+            <option v-for="(item, index) in cityList" :key="index">
+              {{ item }}
+            </option>
+          </select>
         </div>
         <!-- 搜尋 -->
         <div>
-          <div class="search-form rounded mb-2">
+          <!-- <div class="search-form rounded mb-2">
             <input
               type="search"
               id="form1"
@@ -40,12 +36,13 @@
               placeholder="Search"
               aria-label="Search"
             />
-          </div>
+          </div> -->
           <button
             type="button"
-            class="btn btn-info serch-button d-flex rounded"
+            class="btn serch-button d-flex rounded"
+            @click="$router.push({ name: 'SearchResult', params: { city } })"
           >
-            Info
+            搜尋
           </button>
         </div>
       </div>
@@ -55,15 +52,18 @@
   <h2 class="text-title">近期活動</h2>
   <ActiveCard />
   <h2 class="text-title">熱門打卡景點</h2>
-  <HotCard />
+  <HotCard :hotData="hotData" />
 </template>
 
 <script>
 // @ is an alias to /src
+import { reactive, onMounted, ref } from "vue";
 import Navbar from "@/components/Navbar.vue";
 import Swiper from "@/components/Swiper.vue";
 import ActiveCard from "@/components/ActiveCard.vue";
 import HotCard from "@/components/HotCard.vue";
+import { getHotSpot } from "@/apis/index.js";
+import { CityList } from "@/utility/City";
 
 export default {
   name: "Home",
@@ -73,6 +73,45 @@ export default {
     ActiveCard,
     HotCard,
   },
+  setup() {
+    const hotData = ref([]);
+    const cityList = Object.values(CityList);
+    const city = ref("");
+
+    const handleSelect = (city) => {
+      console.log("ddd", city);
+    };
+
+    const getData = async () => {
+      let data;
+      await getHotSpot.get().then((response) => {
+        data = response.map((item) => {
+          if (Object.keys(item.Picture).length !== 0) {
+            return {
+              ScenicSpotID: item.ScenicSpotID,
+              ScenicSpotName: item.ScenicSpotName,
+              Picture: item.Picture.PictureUrl1,
+              Address: item.Address,
+            };
+          }
+        });
+        hotData.value = data.filter((item) => {
+          return item !== undefined;
+        });
+        //console.log(hotData.value);
+      });
+    };
+    onMounted(() => {
+      getData();
+      console.log(city.value);
+    });
+    return {
+      hotData,
+      cityList,
+      handleSelect,
+      city,
+    };
+  },
 };
 </script>
 <style lang="scss" scoped>
@@ -80,20 +119,67 @@ export default {
 .title {
   font-size: 3rem;
 }
-.dropdown {
-  width: 50%;
+.dropdown-section {
+  text-align: left;
+}
+select {
+  background: #003d79;
+  color: #fff;
+  border: 1px solid #e6e6e6;
   margin-bottom: 1rem;
+  width: 50%;
+  height: 3em;
+  padding: 10px;
+  position: relative;
+  appearance: none;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  background-image: url("data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2224%22%20height%3D%2224%22%20viewBox%3D%220%200%2024%2024%22%3E%3Cpath%20fill%3D%22%23ffffff%22%20d%3D%22M7%2010l5%205%205-5z%22%2F%3E%3C%2Fsvg%3E"),
+    linear-gradient(#7f977b, #7f977b);
+  background-image: url("data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2224%22%20height%3D%2224%22%20viewBox%3D%220%200%2024%2024%22%3E%3Cpath%20fill%3D%22%23ffffff%22%20d%3D%22M7%2010l5%205%205-5z%22%2F%3E%3C%2Fsvg%3E"),
+    -webkit-linear-gradient(#003d79, #003d);
+  background-image: url("data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2224%22%20height%3D%2224%22%20viewBox%3D%220%200%2024%2024%22%3E%3Cpath%20fill%3D%22%23ffffff%22%20d%3D%22M7%2010l5%205%205-5z%22%2F%3E%3C%2Fsvg%3E"),
+    -moz-linear-gradient(#003d79, #003d79);
+  background-position: right 0.5em top 50%, 0 0;
+  background-size: contain, cover;
+  background-repeat: no-repeat, repeat;
 }
-.dropdown-btn {
-  width: 100%;
-  background-color: #7f977b;
+
+option {
+  appearance: none;
+  background: #fff;
+  position: absolute;
 }
-.dropdown-menu {
-  width: 100%;
+
+.input-section {
+  width: 50%;
+
+  > input {
+    border: 1px solid #e6e6e6;
+    background-color: #f9f9f9;
+    margin: 15px 15px 15px 0;
+    height: 3em;
+    width: 100%;
+    &::placeholder {
+      padding: 10px;
+    }
+  }
 }
+
+// .dropdown-menu {
+//   width: 100%;
+// }
 .serch-button {
   width: 50%;
   background-color: #7f977b;
+  color: #fff;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    color: #fff;
+    background-color: #515e4f;
+  }
 }
 .secontion-one {
   margin: 80px 0;
